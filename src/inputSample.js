8d71a3e490a346bcbfbf8ca876060ce4e599dfd6
rumblekat <reki318@naver.com>
2021-02-12 23:49:39 +0900

    using useRef

--- src/inputSample.js
+++ src/inputSample.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React ,{useState , useRef} from 'react'; //특정 DOM을 접근하기 위한 useRef
 
 function InputSample(){
     const [inputs, setInputs] = useState({
@@ -6,6 +6,7 @@ function InputSample(){
         nickname : ''
     });//여러개 input 값의 status를 바꿀 때 사용
 
+    const nameInput = useRef();
     const {name,  nickname} = inputs;
 
     const onChange = (e) =>{
@@ -21,11 +22,17 @@ function InputSample(){
             name : '',
             nickname : ''
         })
+        nameInput.current.focus();
     }
+
+    /*
+        1. useRef()를 사용하여 Ref 객체를 만든다.
+        2. 선택하고 싶은 DOM에 매핑하면 .current는 해당 DOM을 가르키게된다. 
+    */
 
     return(
         <>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}  />
             <button onClick={onReset}>초기화</button>
             <div>

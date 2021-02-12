dda2cd196466a9e62786ecbf3bd979ffed56d016
rumblekat <reki318@naver.com>
2021-02-13 01:14:27 +0900

    add useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 
 function User({ user, onRemove,onToggle }) {
+    useEffect(()=>{
+        console.log('user 값이 설정됨')
+        console.log(user);
+        return ()=>{ //clean up 함수 
+            console.log('user가 바뀌기 전...'); //unmount 시 호출 
+            console.log(user)
+        }
+    },[user]);//의존값 배열이 비어있다면, 컴포넌트가 처음 나타날때만 useEffect 에 등록한 함수가 호출됨
+/*
+    useEffect(()=>{
+        console.log(user);
+    })
+    deps 파라미터를 생략했기 때문에 컴포넌트가 리렌더링 될 때마다 호출
+
+    실제 DOM은 바뀐 내용이 있는 컴포넌트만 변경 but Virtual DOM에는 모든 걸다 렌더링한다.
+*/
+
   return (
     <div>
         <b style={{

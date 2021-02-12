2c474fa41a83be407e6314b32a5e38ee765c81ea
rumblekat <reki318@naver.com>
2021-02-12 23:25:55 +0900

    add conditional rendering

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Wrapper from './Wrapper';
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial />
       <Hello color="pink" />  
     </Wrapper>
     );
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Hello(props){
-    return <div style={{color : props.color}}>안녕하세요 {props.name}</div>
+function Hello({color,name,isSpecial}){
+    return (
+        <div style={{color}}>
+            {isSpecial ? <b>*</b>  : null }
+            안녕하세요 {name}    
+        </div>
+    )
 }
 
 Hello.defaultProps = {

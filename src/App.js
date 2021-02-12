79ad40aa64ddd3c33e39d683f729665e19474703
rumblekat <reki318@naver.com>
2021-02-13 00:46:54 +0900

    delete button in array

--- src/App.js
+++ src/App.js
@@ -48,6 +48,11 @@ function App() {
     });
     nextId.current += 1;
   };
+
+  const onRemove = id =>{
+    setUsers(users.filter(user => user.id !== id));
+  }
+
   return (
     <div>
       <CreateUser
@@ -56,7 +61,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove}/>
     </div>
   );
 }

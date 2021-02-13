f3062321c817f61d70df92d49d971c3ec188d95d
rumblekat <reki318@naver.com>
2021-02-13 15:24:52 +0900

    add useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useState, useMemo, useCallback, useReducer } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,20 +7,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    setInputs(inputs => ({
-      ...inputs,
-      [name]: value
-    }));
-  }, []);
-  const [users, setUsers] = useState([
+const initalState = {
+  inputs : {
+    username : '',
+    email : ''
+  },
+  users : [
     {
       id: 1,
       username: 'velopert',
@@ -39,49 +31,90 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ]);
+  ]
+}
 
+function reducer(state, action){
+  switch(action.type){
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name] : action.value
+        }
+      };
+    case 'CREATE_USER':
+      return{
+        inputs : initalState.inputs,
+        users: state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER' :
+      return{
+        ...state,
+        users : state.users.map(user => 
+          user.id === action.id ? {...user, active : !user.active} : user  
+        )
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users : state.users.filter(user => user.id !== action.id)
+      }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+  const { users } = state;
+  const { username, email } = state.inputs;
   const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users => users.concat(user));
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
-    setInputs({
-      username: '',
-      email: ''
+  const onChange = useCallback(e =>{
+    const {name, value} = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  },[]);
+
+  const onCreate = useCallback(()=>{
+    dispatch({
+      type : 'CREATE_USER',
+      user : {
+        id : nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
-  }, [username, email]);
+  },[username,email]);
 
-  const onRemove = useCallback(id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users => users.filter(user => user.id !== id));
-  }, []);
-  const onToggle = useCallback(id => {
-    setUsers(users =>
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  }, []);
-  const count = useMemo(() => countActiveUsers(users), [users]);
+  const onToggle = useCallback(id=>{
+    dispatch({
+      type : 'TOGGLE_USER',
+      id
+    })
+  },[]);
+
+  const onRemove = useCallback(id=>{
+    dispatch({
+      type : 'REMOVE_USER',
+      id
+    })
+  },[]);
+
   return (
     <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성사용자 수 : {count}</div>
     </>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Counter'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // <Counter/>,
   document.getElementById('root')
 );
 
